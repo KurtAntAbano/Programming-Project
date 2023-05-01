1ae29eee0a36d17693127443830b0e0dc5a2d258
KurtAntAbano <ka04122005@gmail.com>
2023-05-01 14:49:25 +0100

    added GUI login, user data and a bunch of other validations

--- myValidation.py
+++ myValidation.py
@@ -1,17 +1,41 @@
-
-
 def isValidLength(text, length, s):
     x = isinstance(text, str)
     if x:
-        if s == "0":
-            if len(text) == length:
-                return True
-            else:
-                return False
+        match s:
+            case "0":
+                if len(text) == length:
+                    return True
+                else:
+                    return False
 
-        elif s == "1":
-            if len(text) > length:
-                return True
-            else:
-                return False
+            case "1":
+                if len(text) > length:
+                    return True
+                else:
+                    return False
 
+
+def prescenceCheck(text):
+    if text == " ":
+        return False
+    else:
+        return True
+
+
+def rangeCheck(text, lm, ul):
+    if lm < len(text) < ul:
+        return True
+    else:
+        return False
+
+def formatCheck(text):
+    if len(text) == 10:
+        if text[2] == "/" and text[5] == "/":
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def emailAlreadyExist()
