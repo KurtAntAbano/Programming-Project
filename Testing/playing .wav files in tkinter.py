0fd31e76911a632b6ac53a9de4a5d7b29d5c5c18
KurtAntAbano <ka04122005@gmail.com>
2023-06-26 18:53:14 +0100

    experimenting on how to play wav and mp3 files on tkinter

--- Testing/playing .wav files in tkinter.py
+++ Testing/playing .wav files in tkinter.py
@@ -1,10 +1,27 @@
+# Problems: small latency, repeatedly pressing a note does not sync
+# possible Solutions: longer presses = longer note duration
+
+
+
 from tkinter import *
 from winsound import *
 
 root = Tk() # create tkinter window
 
 play = lambda: PlaySound('c note.wav', SND_FILENAME)
-button = Button(root, text = 'c note', command = play)
 
+button = Button(root, text = 'c note', command = play)
 button.pack()
+
+
+
+#---------------------------KEY PRESS TEST------------------------------------------
+def playNote(event):
+    PlaySound('c note.wav', SND_FILENAME)
+
+root.bind("<c>",playNote)
+
+
+
+
 root.mainloop()
